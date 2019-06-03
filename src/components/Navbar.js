11441e057169b94dee8b6427d2e241784d44661d
Salman Tariq Lone <salmanlone89@gmail.com>
2019-06-03 18:14:09 +0800

    minor changes and fixes.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom'
  const Navbar = ()=>{
     return(
-            <nav className="nav-wrapper">
+            <nav className="nav-wrapper"> 
                 <div className="container">
-                    <Link to="/" className="brand-logo">*</Link>
+                    <Link to="/" className="brand-logo">Seek Asia Assignment</Link>
                     
                     <ul className="right">
                         <li><Link to="/">Dashboard</Link></li>
